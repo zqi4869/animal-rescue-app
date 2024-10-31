dce63db91eae17f023c7cfa2660bb4f3e46cbc94
zqi4869 <676768172@qq.com>
2024-10-31 21:53:01 +0800

    Http request util

---
+++ page/utils/http.js
@@ -0,0 +1,41 @@
+import { Alert } from 'react-native';
+
+const baseUrl = 'http://192.168.1.169:8080'
+
+function fetchPost({ routeName, dataJson, method = 'POST', success }) {
+  return fetch(baseUrl + routeName, {
+    method,
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(dataJson)
+  })
+    .then(response => response.json())
+    .then(data => {
+      if (data.code === 0) {
+        success(data.data);
+      } else {
+        Alert.alert('Error', data.msg);
+      }
+    }).catch(error => {
+      Alert.alert('Error', error);
+    })
+}
+
+
+function fetchGet(routeName, success) {
+  return fetch(baseUrl + routeName)
+    .then(response => response.json())
+    .then(data => {
+      if (data.code === 0) {
+        console.log(data.data) // todo: debug
+        success(data.data);
+      } else {
+        Alert.alert('Error', data.msg);
+      }
+    }).catch(error => {
+      Alert.alert('Error', error);
+    })
+}
+
+export { fetchPost, fetchGet, baseUrl }
