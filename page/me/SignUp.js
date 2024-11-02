1e07a6a4d1389c8593244146a8385918cb1cb81e
zqi4869 <676768172@qq.com>
2024-11-02 19:46:02 +0800

    Add Sign-up component

--- page/me/SignUp.js
+++ page/me/SignUp.js
@@ -1,26 +1,22 @@
-import React, {useState, useEffect} from 'react';
-import {
-  StyleSheet,
-  Text,
-  View,
-  TouchableOpacity,
-} from 'react-native';
-import {Button, ListItem, Input} from '@rneui/themed';
+import React, { useState } from 'react';
+import { StyleSheet, View, } from 'react-native';
+import {Button, Input} from '@rneui/themed';
 import AntDesignIcon from 'react-native-vector-icons/AntDesign';
+import FileUpload from "../../components/FileUpload";
+import { getImageUri, fetchPost } from "../utils/http";
 
 const SignUp = ({navigation}) => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
-  const [phone, setPhone] = useState('');
 
-  const onRegister = () => {
-    console.log('Login with username:', username, 'and password:', password);
+  const onSubmit = () => {
+    fetchPost('/user/save', {
+      username,
+      password,
+    }, () => {
+      navigation.goBack()
+    })
   };
-
-  const onUpload = () => {
-    console.log('Upload profile picture');
-  };
-
 
   return (
     <View style={styles.container}>
@@ -36,23 +32,11 @@ const SignUp = ({navigation}) => {
           type="password"
           onChangeText={pwd => setPassword(pwd)}
         />
-        <Input
-          label="Phone Number"
-          value={phone}
-          type="number"
-          keyboardType="numeric"
-          onChangeText={phone => setPhone(phone)}
-        />
-        <TouchableOpacity onPress={onUpload}>
-          <View style={styles.upload}>
-            <AntDesignIcon name="upload" size={50}  />
-            <Text>Upload Profile Picture</Text>
-          </View>
-        </TouchableOpacity>
+        <FileUpload />
         <Button
           color="warning"
           icon={<AntDesignIcon name="save" size={20} color="#fff" />}
-          onPress={onRegister}
+          onPress={onSubmit}
         >  Submit</Button>
       </View>
     </View>
@@ -63,22 +47,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    // justifyContent: 'center',
     padding: 10,
   },
   form: {
     width: '100%',
   },
-  upload: {
-    borderWidth: 1,
-    borderColor: '#4b4141',
-    borderRadius: 5,
-    padding: 20,
-    marginBottom: 20,
-    borderStyle: 'dashed',
-    justifyContent: 'center',
-    alignItems: 'center',
-  }
 });
 
 export default SignUp;
